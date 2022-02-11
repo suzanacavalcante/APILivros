1c548b476b9168a1aba49ed9443713bf7fc9a4bd
Suzana <suzanacavalcantepc@gmail.com>
2022-02-11 00:26:09 -0300

    APILivros

---
+++ BookAPI/Model/Books.cs
@@ -0,0 +1,12 @@
+﻿namespace BookAPI.Model
+{
+    public class Books
+    {
+        public int Id { get; set; }
+        public string Author { get; set; }
+        public string Description { get; set; }
+        public string Title { get; set; }
+
+
+    }
+}
